966d4528dd8c6afb6413a8afcfdddb81efbbdf31
humanathome <eabicic@gmail.com>
2022-05-02 18:54:29 +0200

    add different coloring modes functions

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-const gridSizeSlider = document.querySelector('.slider');
-const gridContainer = document.querySelector('.grid-container');
-const controlsContainer = document.querySelector('.controls-container');
-const clearGridBtn = document.querySelector('.reset-grid-btn');
+const gridSizeSlider = document.getElementById('grid-size-slider');
+const gridContainer = document.getElementById('grid-container');
+const clearGridBtn = document.getElementById('clear-grid-btn');
 
 let gridSize = gridSizeSlider.valueAsNumber;
 let totalNumOfCells = gridSize * gridSize;
-let rangeValueDisplay = document.querySelector('.range-slider-value');
+let rangeValueDisplay = document.getElementById('range-slider-value');
 rangeValueDisplay.textContent = gridSize;
+
+let activeMode;
+let activeModeDisplay = document.getElementById('active-mode-display');
+activeModeDisplay.textContent = `${activeMode}`;
 
 // get new slider value and resize grid on slider mouseup
 gridSizeSlider.oninput = getSliderValue;
@@ -29,16 +32,73 @@ function clearGrid() {
 function generateGrid() {
   for (let i=1; i <= totalNumOfCells; i++) {
     const cell = document.createElement('div');
-    cell.classList.add(`grid`);
+    cell.classList.add('grid');
     cell.addEventListener('mouseover', changeBgColor);
     cell.addEventListener('mousedown', changeBgColor);
     gridContainer.appendChild(cell);
   }
+  activeMode = 'rainbow';
+  activeModeDisplay.textContent = `${activeMode}`;
   gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
 }
 
-// generate a random color
+// handle mouse events for coloring cells
+let mouseDown = false;
+gridContainer.addEventListener('mousedown', (e) => {
+  e.preventDefault();
+  mouseDown = true;
+});
+
+gridContainer.addEventListener('mouseup', (e) => {
+  e.preventDefault();
+  mouseDown = false;
+});
+
+// main function for color changing
+function changeBgColor(e) {
+  // stop execution if both mouseover and mousedown events are not present
+  if (e.type === 'mouseover' && !mouseDown) return;
+  if (activeMode === 'eraser') {
+    this.style.backgroundColor = '';
+  } else if (activeMode === 'black') {
+    this.style.backgroundColor = 'rgb(0,0,0)';
+  } else if (activeMode === 'lighten') {
+    this.style.backgroundColor = lighten(e);
+  } else if (activeMode === 'darken') {
+    this.style.backgroundColor = darken(e);
+  } else if (activeMode === 'rainbow') {
+    this.style.backgroundColor = generateRandColor();
+  }
+}
+
+// color functions
+function lighten(e) {
+  let currentColor = e.target.style.backgroundColor;
+  if (currentColor === '') currentColor = 'rgb(255, 255, 255)';
+  const rgbArray = currentColor.match(/\d+/g);
+  let r = Number(rgbArray[0]);
+  let g = Number(rgbArray[1]);
+  let b = Number(rgbArray[2]);
+  r += 25;
+  g += 25;
+  b += 25;
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function darken(e) {
+    let currentColor = e.target.style.backgroundColor;
+    if (currentColor === '') currentColor = 'rgb(255, 255, 255)';
+    const rgbArray = currentColor.match(/\d+/g);
+    let r = Number(rgbArray[0]);
+    let g = Number(rgbArray[1]);
+    let b = Number(rgbArray[2]);
+    r -= 25;
+    g -= 25;
+    b -= 25;
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
 function generateRandColor() {
   const r = Math.floor(Math.random() * 256 );
   const g = Math.floor(Math.random() * 256 );
@@ -46,22 +106,46 @@ function generateRandColor() {
   return `rgb(${r}, ${g}, ${b})`;
 }
 
-// handle mouse events for coloring cells
-let mouseDown = false
-gridContainer.addEventListener('mousedown', (e) => {
-  e.preventDefault();
-  (mouseDown = true);
-});
-
-gridContainer.addEventListener('mouseup', (e) => {
-  e.preventDefault();
-  mouseDown = false
-});
-
-function changeBgColor(e) {
-  // stop execution if both mouseover and mousedown events are not present
-  if (e.type === 'mouseover' && !mouseDown) return;
-  this.style.backgroundColor = generateRandColor();
+// set different coloring modes on click
+function setLightenMode() {
+  activeMode = 'lighten';
+  activeModeDisplay.textContent = `${activeMode}`;
 }
+
+function setDarkenMode() {
+  activeMode = 'darken';
+  activeModeDisplay.textContent = `${activeMode}`;
+}
+
+function setBlackMode() {
+  activeMode = 'black';
+  activeModeDisplay.textContent = `${activeMode}`;
+}
+
+function setEraserMode() {
+  activeMode = 'eraser';
+  activeModeDisplay.textContent = `${activeMode}`;
+}
+
+function setRainbowMode() {
+  activeMode = 'rainbow';
+  activeModeDisplay.textContent = `${activeMode}`;
+}
+
+// mode buttons
+const eraserBtn = document.getElementById('eraser-btn');
+eraserBtn.addEventListener('click', setEraserMode);
+
+const blackBtn = document.getElementById('black-btn');
+blackBtn.addEventListener('click', setBlackMode);
+
+const lightenBtn = document.getElementById('lighten-btn');
+lightenBtn.addEventListener('click', setLightenMode);
+
+const darkenBtn = document.getElementById('darken-btn');
+darkenBtn.addEventListener('click', setDarkenMode);
+
+const rainbowBtn = document.getElementById('rainbow-btn');
+rainbowBtn.addEventListener('click', setRainbowMode);
 
 window.onload = generateGrid;
